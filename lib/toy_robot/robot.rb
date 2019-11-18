167cb4a3b5eb052fd1926cce3ff4346b47bfbc73
Molly Madden <molly.annette.madden@gmail.com>
2019-11-19 10:21:40 +1100

    simple tests now passing

---
+++ lib/toy_robot/robot.rb
@@ -0,0 +1,16 @@
+module ToyRobot
+    class Robot
+        def initialize(position=0)
+        @position = position
+        end
+
+        def move
+        @position += 1
+        end
+
+        def position
+        @position
+        end
+
+    end
+end
