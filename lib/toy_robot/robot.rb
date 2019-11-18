8f63ae738ab3020cc38f30a2c0fa35d3ebf7c611
Molly Madden <molly.annette.madden@gmail.com>
2019-11-19 10:29:10 +1100

    robot now moving east and west

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,15 +1,17 @@
 module ToyRobot
     class Robot
+        attr_reader :position
+
         def initialize(position=0)
         @position = position
         end
 
-        def move
+        def move_east
         @position += 1
         end
 
-        def position
-        @position
+        def move_west
+        @position -= 1
         end
 
     end
